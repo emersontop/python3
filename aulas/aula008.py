24b30a07c653bee8d1b218127253108f2819e969
Guilherme Lourenço <53027213+egmerson@users.noreply.github.com>
2022-06-21 22:21:07 -0300

    exercicios

---
+++ aulas/aula008.py
@@ -0,0 +1,15 @@
+#Aula 008
+#importar bibliotecas podemos importar a biblioteca interia ou so alguns comandos da biblioteca
+#import nome da biblioteca
+#from nome da biblioteca import função
+
+#from math import sqrt, floor
+#n = int(input('Digite um numero: '))
+#raiz = math.sqrt(n)
+#print('A raiz de {} é {}'.format(n,raiz))
+
+#algumas bibliotecas criadas por outras pessoas podem ser importadas, mas você terá que baixar ela para o seu computador
+#para importat bibliotecas que não estõa no seu computador você tem que usar o comando pip install nome da bilioteca. 
+#normalmente no próprio site do puthon.org ja tem o comando. esse comando tem que ser usado no terminal
+import emoji
+print(emoji.emojize('Python is :thumbsup:', language='alias'))
---
+++ exercicios/ex016.py
@@ -0,0 +1,4 @@
+#Quebrando um número
+import math
+num = float(input('digite um número: '))
+print('o valor digitado foi :{} e a parte inteira é: {}'.format(num,math.trunc(num)))
