32f3a2ae5926053cd1b367ee1ecb39aa004a0437
Guilherme Lourenço <53027213+egmerson@users.noreply.github.com>
2022-06-28 21:54:36 -0300

    aulas

---
+++ aulas/aula017.py
@@ -0,0 +1,8 @@
+#Listas []
+#Podem ser mutáveis, ao contrário das tuplas
+#Para adicionar elementos a uma lista, usar o comando .append(o que deseja adicionar) Para adicionar no final
+#Para adicionar em qlqr local use .insert(posição, 'o que deseja adicionar')
+#Para eliminar elementos você pode usar del lanche[posição] ou lanche.pop(posição) ou remove('nome
+#Para testar se algo esta em uma lista coloque if 'algo' in lista
+#Usar o comando. sort() para ordenar a lista
+#Para saber o tamanho da lista usar o comando len()
